68014bfc37530feaa43fe54b2d0c0f00e4b76e87
Higor Henrique Campos de Assis <higheitor@outlook.com>
2020-10-05 00:59:25 -0400

    implementação do card, header e home

---
+++ public/javascript/header.js
@@ -0,0 +1,14 @@
+var nav =  document.getElementById("nav");
+
+function menubar() {
+    
+    
+    if(nav.style.display !== "" && nav.style.display !== "none") {  
+        nav.style.display = "none";
+    }
+    else  {
+      
+        nav.style.display = "flex";
+     
+    }
+}
