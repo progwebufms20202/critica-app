f66e114a70bec1dfc27de284cd557be73d40888c
Emerson Mendes <emersonmesouza@gmail.com>
2020-10-07 18:39:29 -0400

    nova estrutura

--- controllers/movieController.js
+++
@@ -1,9 +0,0 @@
-exports.getMovies = (req, res, next) => {
-  const movies = []
-
-  res.render('home', {
-    movies,
-    path: '/',
-    pageTitle: 'Home',
-  })
-}
--- controllers/userController.js
+++
@@ -1,8 +0,0 @@
-exports.login = (req, res, next) => {
-  
-    res.render('login', {      
-      path: '/',
-      pageTitle: 'Login',
-    })
-}
-
