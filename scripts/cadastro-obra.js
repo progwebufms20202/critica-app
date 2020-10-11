fd109c15bd74e562c7da4c3ebd0583d2c4d74623
Higor Henrique Campos de Assis <higheitor@outlook.com>
2020-10-10 20:07:09 -0400

    preview-image cadastro-obra

--- scripts/cadastro-obra.js
+++ scripts/cadastro-obra.js
@@ -20,14 +20,11 @@ function Cadastrar() {
     }
 
 }
-
-function upload() {
-
-    // const file = document.getElementById("imagemObra");
-    // console.log(file)
-    // const fileReader = new FileReader();
-    // fileReader.onload = function() {
-    //     console.log(fileReader.result);
-    // }
-    // fileReader.readAsDataURL(file)
-}
+function showPreview(event){
+    if(event.target.files.length > 0){
+      var src = URL.createObjectURL(event.target.files[0]);
+      var preview = document.getElementById("file-ip-1-preview");
+      preview.src = src;
+      preview.style.display = "block";
+    }
+  }
